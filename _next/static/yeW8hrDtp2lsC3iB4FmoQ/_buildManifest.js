7437db8f4bcc6999956ecf83fd4d5ea1c4374fee
Harsh Singh <hi.harsh@protonmail.ch>
2021-12-11 14:57:56 -0500

    Upload static files

---
+++ _next/static/yeW8hrDtp2lsC3iB4FmoQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/121-acddccd21c159f58.js","static/chunks/pages/index-22d42b789b548727.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
