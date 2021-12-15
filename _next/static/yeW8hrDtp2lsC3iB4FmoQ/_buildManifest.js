4c9286572a6abf09d0665362f3ae9ed66be2bd4f
Harsh Singh <hi.harsh@protonmail.ch>
2021-12-15 16:54:16 -0500

    Remove agile-ts

--- _next/static/yeW8hrDtp2lsC3iB4FmoQ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/121-acddccd21c159f58.js","static/chunks/pages/index-22d42b789b548727.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
